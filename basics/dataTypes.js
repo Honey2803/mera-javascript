d056c07ed42299a9a4509d84e2bdfb1a800db12f
Honey2803 <79039554+Honey2803@users.noreply.github.com>
2023-12-21 10:53:29 +0000

    data ki types

---
+++ basics/dataTypes.js
@@ -0,0 +1,14 @@
+/*let someNum = 33
+let stringNum = String(someNum);
+console.log(stringNum);
+console.log(typeof stringNum);
+*/
+
+let value = 3
+let negValue = -value
+//console.log(negValue);
+
+console.log(2/3);
+console.log("5"+4);
+console.log("1"+5+7);
+console.log(1+2+"8");
