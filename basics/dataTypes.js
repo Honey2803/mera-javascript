f4eefe88f2a7a09836feb0d7b4ec94105499acc7
Honey2803 <79039554+Honey2803@users.noreply.github.com>
2023-12-21 17:54:41 +0000

    newww

--- basics/dataTypes.js
+++ basics/dataTypes.js
@@ -12,3 +12,26 @@ console.log(2/3);
 console.log("5"+4);
 console.log("1"+5+7);
 console.log(1+2+"8");
+
+// Primitive
+
+// 7 types : String,Number,Boolean,NUll,undefined,Symbol,BigInt
+//use of symbol
+const id = Symbol('113')
+
+// Reference (Non primitive)
+
+//Array, Objects,Functions
+
+const heros=["spidey","ironman","capt"] //array
+
+let myObj ={
+    name:"hitesh",
+    age:22,
+} //obj
+
+
+const myFunction = function(){
+    console.log("Hello");
+}
+
